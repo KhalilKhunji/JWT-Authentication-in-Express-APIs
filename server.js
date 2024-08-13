de243a4141dc3ea9f951d7cc9397dfa88525e70e
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-13 10:26:49 +0300

    setup complete

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 require('./config/database.js');
+const morgan = require('morgan');
 
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
 app.use(express.json());
 
-// Routes go here
 
-app.listen(3000, () => {
+// Routes
+
+app.listen(port, () => {
   console.log('The express app is ready!');
 });
