c98897722ae99702811a17ae6da8a504c6a0db99
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-13 13:00:33 +0300

    sign in complete

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user.js');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req, res) => {
     try {
@@ -15,7 +16,23 @@ router.post('/signup', async (req, res) => {
             username,
             hashedPassword
         });
-        return res.status(201).json({ user });
+        const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+        return res.status(201).json({ user, token });
+    } catch (error) {
+        res.status(400).json({ error: 'Something went wrong! Try again.' });
+    };
+});
+
+router.post('/signin', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (user && bcrypt.compareSync(password, user.hashedPassword)) {
+            const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+            res.status(200).json({ token });
+        } else {
+            res.status(401).json({ error: 'Something went wrong! Try again.' });
+        }
     } catch (error) {
         res.status(400).json({ error: 'Something went wrong! Try again.' });
     };
