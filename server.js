e11c674864f878ab9d95d635ca27cd297aed0344
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-13 11:28:46 +0300

    sign and verify token

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 require('./config/database.js');
 const morgan = require('morgan');
 
+// Controllers
+const testJWTRouter = require('./controllers/test-jwt');
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
@@ -11,6 +13,7 @@ app.use(express.json());
 
 
 // Routes
+app.use('/test-jwt', testJWTRouter);
 
 app.listen(port, () => {
   console.log('The express app is ready!');
